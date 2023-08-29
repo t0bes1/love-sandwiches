5d2043b910d4d981faa8ca76979770c0cfec4e66
t0bes1 <131770004+t0bes1@users.noreply.github.com>
2023-08-29 11:46:02 +0000

    Add update surplus worksheet

--- run.py
+++ run.py
@@ -64,12 +64,19 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def update_surplus_worksheet(data):
+    print("updating surplus worksheet ...")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("surplus worksheet updated successfully")
+
+
 def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 
 print("Welcome to Love Sandwiches data automation")
