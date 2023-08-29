9b1c875a40489b12160aa9f9ef3993f35dab8ccc
t0bes1 <131770004+t0bes1@users.noreply.github.com>
2023-08-29 12:34:03 +0000

    Deployment

--- run.py
+++ run.py
@@ -3,7 +3,6 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -21,7 +20,7 @@ def get_sales_data():
     while True:
         print("please enter sales data")
         print("data should be 6 numbers")
-        data_str = input("Enter your data here:")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
         if validate_data(sales_data):
